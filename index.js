0c643ada8b1caa739131f04815dadf5f2a719522
MungaiS <thukusteven@gmail.com>
2023-12-14 13:15:32 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback){
+  callback()
+
+}
+function returnsANamedFunction () {
+    function fn () {
+    }
+    return fn 
+}
+
+function returnsAnAnonymousFunction () {
+    return function () {
+
+    }
+
+  }
